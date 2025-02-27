66d198ecf8f1784787703be85796893fd6af07d2
bangjuyoung <juyoungbang82@gmail.com>
2025-02-27 16:23:38 +0900

    chore(deps): update npm outdated

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  output: 'export',
+  transpilePackages: [
+    // @see https://github.com/vercel/next.js/issues/64525
+    'next-mdx-remote',
+  ],
 };
 
 export default nextConfig;
