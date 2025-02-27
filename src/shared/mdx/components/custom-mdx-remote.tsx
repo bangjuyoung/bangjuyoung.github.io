6eddeb65f1901557a53173678a9d21609a40174a
bangjuyoung <juyoungbang82@gmail.com>
2025-02-27 21:26:10 +0900

    chore(deps): bump tailwindcss version to 4.0.9
    ref. https://tailwindcss.com/docs/installation/using-vite

--- src/shared/mdx/components/custom-mdx-remote.tsx
+++ src/shared/mdx/components/custom-mdx-remote.tsx
@@ -2,18 +2,20 @@ import { MDXRemote, type MDXRemoteProps } from 'next-mdx-remote/rsc';
 import remarkGfm from 'remark-gfm';
 import { highlight } from 'sugar-high';
 
-import './custom-mdx-remote.css';
-
 const MDX_COMPONENTS: MDXRemoteProps['components'] = {
-  code: ({ children, ...props }) => {
+  a: ({ className, ...props }) => {
+    return <a className={`underline-offset-4 text-blue-600 hover:text-blue-500 ${className}`} {...props} />;
+  },
+  code: ({ children, className, ...props }) => {
     const codeHTML = highlight(children);
-    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
+
+    return <code dangerouslySetInnerHTML={{ __html: codeHTML }} className={`code-highlight ${className}`} {...props} />;
   },
 };
 
 export function CustomMDXRemote({ options, ...props }: MDXRemoteProps) {
   return (
-    <article className="custom-mdx-remote">
+    <article className="max-w-none prose prose-slate">
       <MDXRemote
         {...props}
         components={{ ...MDX_COMPONENTS, ...props?.components }}
