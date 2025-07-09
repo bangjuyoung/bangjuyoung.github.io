5b7974c728a57525158fbcb50f47db26861d28d6
bangjuyoung <juyoungbang82@gmail.com>
2025-07-10 00:01:36 +0900

    chore: rename components to ui for clarity

--- src/modules/common/i18n/index.ts
+++ src/modules/common/i18n/index.ts
@@ -1,6 +1,6 @@
 export * from './application';
-export * from './components';
 export * from './config';
 export * from './domain';
 export * from './infrastructure';
 export * from './lib';
+export * from './ui';
